90e6ba32077071956da27523f22faedc07b8b269
matt1031137 <s1001754@gm.pu.edu.tw>
2025-03-14 00:51:50 +0800

    修bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("three")) return "three"; // 把 Three.js 分成一個 chunk
-            if (id.includes("react")) return "react"; // 把 React 相關模組分成一個 chunk
-            return "vendor"; // 其他第三方套件統一放 vendor
-          }
-        },
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
+
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes("node_modules")) {
+//             if (id.includes("three")) return "three"; // 把 Three.js 分成一個 chunk
+//             if (id.includes("react")) return "react"; // 把 React 相關模組分成一個 chunk
+//             return "vendor"; // 其他第三方套件統一放 vendor
+//           }
+//         },
+//       },
+//     },
+//   },
+// });
